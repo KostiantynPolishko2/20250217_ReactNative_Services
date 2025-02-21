4059fde5b7860e4c8b568590f1ebe88c384b28e5
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2025-02-21 23:24:42 +0200

    3 was connected scratch functionality to fetch data of contacts.

--- react-native-services/app/components/contacts/contacts-list.tsx
+++ react-native-services/app/components/contacts/contacts-list.tsx
@@ -1,14 +1,26 @@
-import React, { FC } from "react";
-import { View, Text } from "react-native";
+import React, { FC, useState, useEffect } from "react";
+import { View, Text, ScrollView } from "react-native";
 import { ListStyles } from "./contacts-styles";
+import * as Contacts from "expo-contacts";
 
 const ContactsList: FC = () => {
+
+    const [contacts, setContacts] = useState<Contacts.Contact[] | undefined>(undefined)
+
+    useEffect(()=>{
+        (async () => {
+            const { data } = await Contacts.getContactsAsync({fields: [Contacts.Fields.PhoneNumbers, Contacts.Fields.Emails, Contacts.Fields.Image]});
+            setContacts(data);
+            // console.log('contacts', contacts && contacts[0]);
+        })
+        ();
+    }, []);
+
     return(
         <View style={ListStyles.body}>
-            <Text>1 contact list</Text>
-            <Text>2 contact list</Text>
-            <Text>3 contact list</Text>
-            <Text>4 contact list</Text>
+            <ScrollView>
+                {contacts && contacts.map((contact, index)=>(<Text key={contact.id}>{contact.firstName}</Text>))}
+            </ScrollView>
         </View>
     );
 }
