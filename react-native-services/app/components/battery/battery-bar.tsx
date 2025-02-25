7b9c64e33c5c8f6fdab43a00ae539a6f1bf7cc75
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2025-02-25 17:20:47 +0200

    5 was connected vibration to FC  BatteryBar

--- react-native-services/app/components/battery/battery-bar.tsx
+++ react-native-services/app/components/battery/battery-bar.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState, useEffect } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Vibration } from "react-native";
 import * as Battery from 'expo-battery';
 import { BatteryBarWrapper, CenterPosition, RectangleBox } from "./battery.styled";
 import { ChargeLevelStyle } from "./battery.styled";
@@ -10,8 +10,16 @@ interface IBatteryBar {
 
 const BatteryBar: FC<IBatteryBar> = ({handleIsStats}) => {
 
+    const ONE_SECONDS_IN_MS = 1000;
+    const VIBRO_PATTERN = [
+        1 * ONE_SECONDS_IN_MS,
+        2 * ONE_SECONDS_IN_MS,
+        1 * ONE_SECONDS_IN_MS,
+    ]
+
     const [isAvailable, setIsAvailable] = useState<boolean>(false);
     const [_batteryLevel, setBatteryLevel] = useState<number>(0);
+    const [isVibro, setIsVibro] = useState<boolean>(false);
 
     const level:number = Math.round(Battery.useBatteryLevel() * 100);
 
@@ -33,9 +41,14 @@ const BatteryBar: FC<IBatteryBar> = ({handleIsStats}) => {
         return () => subscription.remove(); // Clean up listener when unmounted
     }, []);
 
+    useEffect(()=>{
+        if(isVibro){
+            Vibration.vibrate(VIBRO_PATTERN);
+        }
+    }, [isVibro]);
 
     return (
-        <TouchableOpacity onPress={handleIsStats} disabled={!isAvailable}>
+        <TouchableOpacity onPress={()=>{handleIsStats(); setIsVibro(!isVibro);}} disabled={!isAvailable}>
             <CenterPosition>
                 <RectangleBox/>
                 <BatteryBarWrapper>
--- react-native-services/app/components/battery/battery-stats.tsx
+++ react-native-services/app/components/battery/battery-stats.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Vibration, TouchableOpacity } from 'react-native';
 import { BatteryStatsStyles as styles } from './battery.styled';
 
 export interface IStats {
@@ -17,22 +17,20 @@ const BatteryStats: FC<IBatteryState> = ({batteryState}) => {
   const states: string[] = ['unknown', 'unplugged', 'charging', 'full'];
 
     return(
-      // <SafeAreaView>
-          <View style={[styles.stats, styles.container]}>
-              <View style={[styles.statsItem, {borderLeftWidth: 0} ]}>
-                <Text style={styles.statsItemLabel}>level</Text>
-                <Text style={styles.statsItemValue}>{batteryState.level}</Text>
-              </View>
-              <View style={[styles.statsItem]}>
-                <Text style={styles.statsItemLabel}>power moode</Text>
-                <Text style={styles.statsItemValue}>{batteryState.lowPowerMode? 'on' : 'off'}</Text>
-              </View>
-              <View style={[styles.statsItem, {borderRightWidth: 0} ]}>
-                <Text style={styles.statsItemLabel}>state</Text>
-                <Text style={styles.statsItemValue}>{states[batteryState.state]}</Text>
-              </View>
+      <View style={[styles.stats, styles.container]}>
+          <View style={[styles.statsItem, {borderLeftWidth: 0} ]}>
+            <Text style={styles.statsItemLabel}>level</Text>
+            <Text style={styles.statsItemValue}>{batteryState.level}</Text>
           </View>
-      // </SafeAreaView>
+          <View style={[styles.statsItem]}>
+            <Text style={styles.statsItemLabel}>power moode</Text>
+            <Text style={styles.statsItemValue}>{batteryState.lowPowerMode? 'on' : 'off'}</Text>
+          </View>
+          <View style={[styles.statsItem, {borderRightWidth: 0} ]}>
+            <Text style={styles.statsItemLabel}>state</Text>
+            <Text style={styles.statsItemValue}>{states[batteryState.state]}</Text>
+          </View>
+      </View>
     );
 }
 
