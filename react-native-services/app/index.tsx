43117152124238b2b41fcfbffd0f89c9810a7be8
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2025-03-07 16:22:58 +0200

    2 FC Card divided on two FC WeaponsModel and WeaponsData

--- react-native-services/app/index.tsx
+++ react-native-services/app/index.tsx
@@ -1,17 +1,23 @@
-import { Text, View } from "react-native";
-import Card from "./components/card";
+import { Text, View, StyleSheet } from "react-native";
+import WeaponsData from "./components/WeaponsData";
+import WeaponsModel from "./components/WeaponsModel";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: '#c9d6cd',
-      }}
-    >
-      <Card/>
+    <View style={styles.body}>
+      <Text>weapons axios request GET</Text>
+      {/* <WeaponsData/> */}
+      <WeaponsModel model="пб"/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#c9d6cd',
+  }
+})
