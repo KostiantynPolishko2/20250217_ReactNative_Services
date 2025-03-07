ae8de5a1497514af5004749fb9c45a5c6f012edc
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2025-03-07 15:08:26 +0200

    2 added  AbortController

--- react-native-services/app/components/card.tsx
+++ react-native-services/app/components/card.tsx
@@ -14,8 +14,11 @@ type WeaponsCardDto = {
 
 const Card: FC = () => {
 
-    const {response, error, loading, treatData} = useAxiosAdmin();
-    const [weapons, setWeapons] = useState<WeaponsCardDto | undefined>(undefined);
+    // const _baseURL = process.env.ASPNET_ADMINSERVER_BASEURL;
+    // console.log('base url', Constants.expoConfig?.extra.ASPNET_ADMINSERVER_BASEURL);
+
+    const {response, error, loading, treatData} = useAxiosAdmin('_baseURL');
+    const [weaponsModel, setWeaponsModel] = useState<WeaponsCardDto | undefined>(undefined);
     const [weaponsData, setWeaponsData] = useState<WeaponsCardDto[] | undefined>(undefined);
 
     // fetch all weapons models
@@ -24,29 +27,35 @@ const Card: FC = () => {
             url: 'client-models',
             method: 'GET',
         });
-        // console.log('get weapons', response || error);
-        setWeaponsData(response);
-    }
-
-    // fetch weapons by model
-    const getWeaponsModel = () => {
-        treatData({
-            url: 'model/ПМ',
-            method: 'GET',
-        });
-        console.log('get weapons', response || error);
+        console.log('get weapons', response? response[0] : error);
     }
 
     useEffect(()=>{
-        setWeapons(response);
-    }, [response])
+        setWeaponsData(response);
+    }, [response]);
+
+
+    // fetch weapons by model
+    // const getWeaponsModel = () => {
+    //     treatData({
+    //         url: 'model/ПБ',
+    //         method: 'GET',
+    //     });
+    //     // console.log('get weapons', response || error);
+    // }
+
+    // useEffect(()=>{
+    //     setWeaponsModel(response);
+    // }, [response])
 
     return (
         <View>
             <Text>cards</Text>
-            <Button title="get" onPress={getWeaponsModel}/>
+            <Button title="GetAll" onPress={getWeaponsAll}/>
+            {weaponsData && <Text>1. {weaponsData[0].model} - {weaponsData[0].price}</Text>}
+            {/* <Button title="GetModel" onPress={getWeaponsModel}/> */}
+            {/* {weaponsModel && <Text>{weaponsModel?.name}</Text>} */}
             {loading && <Text>...loading</Text>}
-            {weapons && <Text>{weapons?.name}</Text>}
         </View>
     );
 }
