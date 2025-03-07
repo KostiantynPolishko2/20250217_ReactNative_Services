5d674c9e23fb371d6de220e49c541c9adad0d1a4
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2025-03-07 15:38:25 +0200

    1 reconnect axios logic

---
+++ react-native-services/app/components/card.tsx
@@ -0,0 +1,64 @@
+import React, { FC, useState, useEffect } from "react";
+import { View, Text, Button } from "react-native";
+import useAxiosAdmin from "../hooks/useAxiosAdmin";
+
+type WeaponsCardDto = {
+    model: string,
+    name: string,
+    isVisible: boolean,
+    price: number,
+    weight: number,
+    description: string,
+    image_path: string,
+}
+
+const Card: FC = () => {
+
+    // const _baseURL = process.env.ASPNET_ADMINSERVER_BASEURL;
+    // console.log('base url', Constants.expoConfig?.extra.ASPNET_ADMINSERVER_BASEURL);
+    const _baseURL = 'http://adminpageserverpl2-dev.eba-pahpkfjc.eu-west-3.elasticbeanstalk.com/api/WeaponsItems';
+
+    const {response, error, loading, treatData} = useAxiosAdmin(_baseURL);
+    const [weaponsModel, setWeaponsModel] = useState<WeaponsCardDto | undefined>(undefined);
+    const [weaponsData, setWeaponsData] = useState<WeaponsCardDto[] | undefined>(undefined);
+
+    // fetch all weapons models
+    const getWeaponsAll = () => {
+        treatData({
+            url: 'client-models',
+            method: 'GET',
+        });
+        console.log('get weapons', response? response[0] : error);
+    }
+
+    useEffect(()=>{
+        setWeaponsData(response);
+    }, [response]);
+
+
+    // fetch weapons by model
+    // const getWeaponsModel = () => {
+    //     treatData({
+    //         url: 'model/ПБ',
+    //         method: 'GET',
+    //     });
+    //     // console.log('get weapons', response || error);
+    // }
+
+    // useEffect(()=>{
+    //     setWeaponsModel(response);
+    // }, [response])
+
+    return (
+        <View>
+            <Text>cards</Text>
+            <Button title="GetAll" onPress={getWeaponsAll}/>
+            {weaponsData && <Text>1. {weaponsData[0].model} - {weaponsData[0].price}</Text>}
+            {/* <Button title="GetModel" onPress={getWeaponsModel}/> */}
+            {/* {weaponsModel && <Text>{weaponsModel?.name}</Text>} */}
+            {loading && <Text>...loading</Text>}
+        </View>
+    );
+}
+
+export default Card;
---
+++ react-native-services/app/hooks/useAxiosAdmin.ts
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+
+const useAxiosAdmin = (_baseURL: string) => {
+    const [response, setResponse] = useState<any | undefined>(undefined);
+    const [error, setError] = useState<string | undefined>(undefined);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const axiosInstance = axios.create({
+        baseURL: _baseURL,
+    });
+
+    let controller = new AbortController();
+
+    // to abort axios request when parent component is unmounted
+    useEffect(()=>{
+        return () => controller?.abort();
+    }, []);
+
+    const treatData = async(item:{url: string, method: string}):Promise<void> => {
+        setLoading(true);
+
+        controller.abort();
+        controller = new AbortController();
+
+        // set up a timeout to abort the request after 10 seconds
+        const timeoutId = setTimeout(()=>{
+            controller.abort();
+        }, 5000); // 5 seconds
+
+        try{
+            const result = await axiosInstance({
+                url: item.url,
+                method: item.method,
+                signal: controller.signal,
+            });
+            
+            clearTimeout(timeoutId); // clear timeout if request completes
+            setResponse(result.data);
+            // console.log('result', result.data);
+        } 
+        catch(error){
+            clearTimeout(timeoutId); // ensure timeout is cleared in case of an error
+
+            if(axios.isCancel(error)){
+                setError('axios request was aborted!');
+            }
+            else{
+                setError('undefined error');
+            }
+        } 
+        finally {
+            setLoading(false);
+        }
+    }
+
+    return {response, error, loading, treatData}
+}
+
+export default useAxiosAdmin;
